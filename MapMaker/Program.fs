2ff654e00563152856dd5ea26a7646f0d7663959
Chad Zawistowski <chad.zawistowski@gmail.com>
2015-07-14 00:53:11 -0700

    1.0ish

--- MapMaker/Program.fs
+++ MapMaker/Program.fs
@@ -5,46 +5,55 @@ open System.Net
 open System.IO
 open System.Windows.Forms
 open System.Threading
+open MathNet.Numerics.Distributions
 
 
 [<EntryPoint>]
 do
     let saveDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
                                      "map_images")
-    let longitudeStart  = 39.62
-    let latitudeStart = -104.9
+    let longitudeStart  = 42.779399
+    let latitudeStart = 76.097755
 
-    for yy in 1 .. 4 do
+    // For random wait times between requests
+    let normalDist = new Normal(600.0, 100.0) // appx 300-900 seconds (5-15 minutes)
+
+    for yy in 0 .. 9 do
         let longitude = longitudeStart - (2.60 * (float)yy)
 
-        for xx in 1 .. 4 do
+        printfn "Row %i -----------------------------------------------------" yy
+
+        for xx in 0 .. 17 do
             let latitude = latitudeStart + (3.30 * (float)xx)
 
             let url = "https://maps.googleapis.com/maps/api/staticmap?"
-                      + "&" + (sprintf "center=%f, %f" longitude latitude)
-                      + "&" + "size=640x640"
-                      + "&" + "scale=2"
-                      + "&" + "maptype=terrain"
-                      + "&" + "zoom=8"
+                      + (sprintf "&center=%f, %f" longitude latitude)
+                      + "&size=640x640"
+                      + "&scale=2"
+                      + "&maptype=terrain"
+                      + "&zoom=8"
+
+            printfn "%s" url
 
             let mapRequest = HttpWebRequest.CreateHttp(url)
             let response = mapRequest.GetResponse()
             let dataStream = match response.ContentType with
-                             | "image/png" -> response.GetResponseStream()
-                             | _ -> raise (new Exception())
+                                | "image/png" -> response.GetResponseStream()
+                                | _ -> raise (new Exception())
 
             let wholeBitmap = Bitmap.FromStream(dataStream) :?> Bitmap
 
             // crop out Google logo
             let croppedImg = wholeBitmap.Clone(new Rectangle(0, 0, wholeBitmap.Width,
-                                                             int(float(wholeBitmap.Height) * 0.957)),
-                                               Imaging.PixelFormat.DontCare)
+                                                                int(float(wholeBitmap.Height) * 0.957)),
+                                                Imaging.PixelFormat.DontCare)
 
             if Directory.Exists(saveDirectory) then
-                croppedImg.Save(Path.Combine(saveDirectory, (sprintf "img_y%i_x%i.png" yy xx)),
+                croppedImg.Save(Path.Combine(saveDirectory, (sprintf "iimg_y%i_x%i.png" yy xx)),
                                 ImageFormat.Png) |> ignore
             else
                 raise (new Exception())
 
-            // rate limit or Google hates you
-            Thread.Sleep(3000)
+            // rate limit or Google will blacklist you
+            let secondsWait = normalDist.Sample()
+            Thread.Sleep((int)(1000.0 * secondsWait))
